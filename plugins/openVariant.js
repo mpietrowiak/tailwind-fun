2666a196153372f5bb6b6c64d7a23a61cc4d290f
Mateusz Pietrowiak <matas8@protonmail.com>
2024-06-07 18:49:51 +0200

    opening mobile menu

--- plugins/openVariant.js
+++ plugins/openVariant.js
@@ -2,6 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 const openVariant = plugin(function ({ addVariant, e }) {
   addVariant("group-open", ":merge(.group).open &");
+  addVariant("peer-open", ":merge(.peer).open ~ &");
 });
 
 module.exports = openVariant;
